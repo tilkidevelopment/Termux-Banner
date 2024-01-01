607bc8ea6cc2ac0f1937e5f2553396e835e4bc34
Tilki <121335160+tilkidevelopment@users.noreply.github.com>
2024-01-01 18:35:02 +0300

    Create basla.sh

---
+++ basla.sh
@@ -0,0 +1,25 @@
+clear
+
+toilet -f big "Kurulum" -F gay | lolcat
+
+echo -e "
+Senin Banner Adın Ne? :
+"
+read varbanner
+
+echo
+
+echo -e "
+Ejderha ne demesini istiyorsun? :
+ "
+read varejder
+
+echo  "cowsay -f dragon "$varcejder" | lolcat" > ejder.txt
+echo "toilet -f big ' $varbanner' -F gay | lolcat" > banner.txt
+echo
+echo "clear" > temizle.txt
+cat "ejder.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+cat "temizle.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+ls
+cat "banner.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+cowsay -f dragon "Kurulum Tamam!, exit yaz." | lolcat
